c09bb3100f9e6b54a2fcad2e11407821866bd2ed
Aperaine <Aperaine@users.noreply.github.com>
2024-09-16 15:48:50 +0900

    Added a cake
    also updated tilemap

---
+++ scripts/cake.gd
@@ -0,0 +1,15 @@
+extends Area2D
+@onready var locked_sprite_2d: Sprite2D = $LockedSprite2D
+@onready var unlocked_sprite_2d: Sprite2D = $UnlockedSprite2D
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+
+
+func Unlocked():
+	locked_sprite_2d.visible = false
+	unlocked_sprite_2d.visible = true
+	collision_shape.disabled = false
+	print("Unlocked")
+
+
+func _on_body_entered(_body: Node2D) -> void:
+	print("win")
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,7 +1,11 @@
 extends Node
-
+@onready var cake_object: Area2D = %CakeObject
 var score = 0
 
 func AddPoint():
 	score+=1
 	print("Score: ", score)
+
+func _process(_delta: float) -> void:
+	if score == 100:
+		cake_object.Unlocked()
