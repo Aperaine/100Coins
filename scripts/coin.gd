429d52133d2629adbe9d37136140b42a88b92e8b
Aperaine <Aperaine@users.noreply.github.com>
2024-09-16 14:47:23 +0900

    Finished left side
    Also did bug fixing (removed all the errors)

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -3,6 +3,6 @@ extends Area2D
 @onready var game_manager: Node = %"Game Manager"
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered() -> void:
 	game_manager.AddPoint()
 	animation_player.play("pickup")
